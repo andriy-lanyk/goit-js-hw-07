633a54c6ebe450d85bd809a0cba5339ec721dbbb
andriy-lanyk <LAM1988@ukr.net>
2021-04-21 11:12:22 +0300

    Выполнил задание 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,10 @@
-const numberOfCategories = document.querySelector('#categories');
+const categories = document.querySelectorAll('.item');
+console.log(`В списке ${categories.length} категории.`);
+
+for (const element of categories) {
+    const numberOfElementsInCategories = element.querySelectorAll('li').length;
+    const categoriesTitle = element.firstElementChild.textContent
+    
+    console.log(`Категория: ${categoriesTitle}, количество элементов: ${numberOfElementsInCategories}`)
+}
+
